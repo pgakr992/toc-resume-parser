8feae8b82ecb03320b8a31024f85a36f2ba56b29
Pgakr992 <prathitagowri@gmail.com>
2025-07-23 18:14:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ def parse_resume_from_path():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=3000)
 
 
