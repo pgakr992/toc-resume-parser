a7f84f61e60a34c90bb646fd375fdabf4cdb88e2
Pgakr992 <prathitagowri@gmail.com>
2025-07-23 17:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,6 @@ def parse_resume_from_path():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-docker run -p 8000:5000 -e FLASK_APP=app.py resume-parser flask run --host=0.0.0.0 --port=5000
+if __name__ == "__main__":
+    app.run(debug=True)
 
